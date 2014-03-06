e3711c3c81ce8c3f57b39d0b74e7d2798a7a05f2
awsam <xyu.wang.0713@gmail.com>
2014-03-06 14:47:26 +0800

    change new post page add editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
+//= require bootstrap-wysiwyg.min
+//= require jquery.hotkeys
 console.log("啊..你在看我代码...");
 
 setTimeout(function(){  $(".flash_messages").fadeOut(); }, 3000);
